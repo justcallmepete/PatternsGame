6c2dcd47e5a91fe94fb7772afa9641939336582c
Weikang Hu <weikang_hu90@hotmail.com>
2017-09-25 16:58:03 +0200

    Added 5 difference bushes, Player becomes semi-transparent when entering a bush, Player has a boolean called 'isHiding', Bush cuts out when player enters, Bushes material tiling is random, resulting in different bushes.

---
+++ Assets/Bush.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour {
+
+    [Tooltip("Set alpha when in normal state.")]
+    public float alphaNormal;
+    [Tooltip("Set alpha when in hiding state.")]
+    public float alphaTransparent;
+    [Tooltip("Set minimal tiling.")]
+    public float minTiling = 0.5f;
+    [Tooltip("Set maximal tiling.")]
+    public float maxTiling = 1.5f;
+
+    private Material[] mat;
+	// Use this for initialization
+	void Start () {
+        mat = gameObject.GetComponent<MeshRenderer>().materials;
+        float scaleX = Random.Range(0.5f, 1.5f);
+        float scaleY = Random.Range(0.5f, 1.5f);
+        mat[0].mainTextureScale = new Vector2(scaleX, scaleY);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    { 
+        if (other.tag == "Player")
+        {
+            SetObjectAlpha(alphaTransparent);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SetObjectAlpha(alphaNormal);
+        }
+    }
+
+    // Change transparency
+    private void SetObjectAlpha(float alpha)
+    {
+        for (int i = 0; i < mat.Length; i++)
+        {
+            Color color = mat[i].color;
+            color.a = alpha;
+            mat[i].color = color;
+            Debug.Log(color.a);
+        }
+    }
+}
---
+++ Assets/PlayerHide.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHide : MonoBehaviour {
+
+    private bool isHiding = false;
+    private MeshRenderer mesh;
+
+	// Use this for initialization
+	void Start () {
+        // Get meshrenderer
+        mesh = gameObject.GetComponentInChildren<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Hideable")
+        {
+            // Become invisible when hiding
+            isHiding = true;
+            SetObjectAlpha(mesh.materials, 0.2f);        
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hideable")
+        {
+            // Become visibile when hiding
+            isHiding = false;
+            SetObjectAlpha(mesh.materials);
+        }
+    }
+
+    // Change transparency
+    private void SetObjectAlpha(Material[] mat, float alpha = 1f)
+    {
+        for (int i = 0; i < mat.Length; i++)
+        {
+            Color color = mat[i].color;
+            color.a = alpha;
+            mat[i].color = color;
+        }
+    }
+
+    // Getter for isHiding
+    public bool IsHiding { get { return isHiding; } }
+}
