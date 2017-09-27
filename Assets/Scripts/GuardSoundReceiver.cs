3d0acb5c3c07ece39000589122fe0aef0d77bbf9
Jeff Kloos <jeffrey.kloos@gmail.com>
2017-09-27 15:51:54 +0200

    Removed unused usings.

--- Assets/Scripts/GuardSoundReceiver.cs
+++ Assets/Scripts/GuardSoundReceiver.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /*
  * This Class contains the guard logic for responding to sound. (Change state, etc.)
