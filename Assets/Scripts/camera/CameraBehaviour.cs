66bc65ba7837a55e6e2031e9a271152b64198cd6
Erik Oegema <erikoegema@outlook.com>
2017-10-10 17:37:30 +0200

    Added lerps, comments. Finalizing stuff.

--- Assets/Scripts/camera/CameraBehaviour.cs
+++ Assets/Scripts/camera/CameraBehaviour.cs
@@ -1,50 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+/*
+ * This class is used to add behaviour to the camera.  
+ * 
+ */
 
 public class CameraBehaviour : MonoBehaviour
 {
-
+    
     [SerializeField]
-    GameObject target;
+    GameObject target; // The focus point (middle point between the players) 
 
     Transform targetPos;
     Transform camPos;
     Camera camera;
     float distance;
 
+    [Header("The delay in the response of the zoom of the camera (lerp) (higher is less delay)")]
+    [SerializeField]
+    [Range(0, 5)]
+    float speed = 2.5f;
+
+    [SerializeField]
+    [Range(1, 2)]
+    float zoomSpeed = 1.125f;
+
     private void Awake()
     {
-        gameObject.GetComponent<Transform>().position = new Vector3(-20, 50, -20);
+        // Defining the start position of the camera
+        gameObject.GetComponent<Transform>().position = new Vector3(-20, 50, -20); 
     }
+
     private void Start()
     {
         targetPos = target.GetComponent<Transform>();
         camPos = gameObject.GetComponent<Transform>();
         camera = gameObject.GetComponent<Camera>();
-
+        target = GameObject.Find("Camera");
     }
-    // Update is called once per frame
+
     void Update()
     {
+        
 
         camPos.LookAt(targetPos);
 
         distance = target.GetComponent<MiddlePointBetweenPlayers>().getDistanceBetweenPlayers();
 
-        camera.fieldOfView = distance * 1.125f;
+        // Zooming in and out, adding lerp effect to it. 
+        float step = speed * Time.deltaTime;
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, camera.fieldOfView = distance * zoomSpeed, step);
 
-        if (camera.fieldOfView < 35)
+
+        if (camera.fieldOfView < 30)
         {
-            camera.fieldOfView = 35;
+            camera.fieldOfView = 30;
         }
         else if (camera.fieldOfView > 60)
         {
             camera.fieldOfView = 60;
-        }
-        else
-        {
-            
         }
 
 
--- Assets/Scripts/camera/MiddlePointBetweenPlayers.cs
+++ Assets/Scripts/camera/MiddlePointBetweenPlayers.cs
@@ -1,32 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/*
+
+    Class is used to define the middle point between the players.
+
+*/
 public class MiddlePointBetweenPlayers : MonoBehaviour {
 
-    public GameObject player1;
-    public GameObject player2;
+    [SerializeField]
+    GameObject player1;
+    [SerializeField]
+    GameObject player2;
 
     Transform posP1;
     Transform posP2;
     Vector3 MiddlePoint;
 
-	// Use this for initialization
+    [Header ("The delay in the response of the position of the camera (lerp) (higher is less delay)")]
+    [SerializeField]
+    [Range(0, 5)]
+    float speed = 2.5f;
+
+
 	void Start () {
 
         posP1 = player1.GetComponent<Transform>();
         posP2 = player2.GetComponent<Transform>();
-
        
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
+        // Adding a lerp effect while defining the middle.
+        float step = speed * Time.deltaTime;
         MiddlePoint = (posP1.position + posP2.position) / 2;
-        gameObject.GetComponent<Transform>().position = MiddlePoint;
+        transform.position = Vector3.Lerp(transform.position, MiddlePoint, step);
+
+   
 	}
 
+    // Getter for distance between the players 
     public float getDistanceBetweenPlayers()
     {
         float dis;
