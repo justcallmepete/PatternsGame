f37a2727dbf49765ea522f6001564d1709370564
Erik Oegema <erikoegema@outlook.com>
2017-10-10 13:26:19 +0200

    Camera behaviours: looks at middle point, has a fixed position from the middle point, zooms in and out.

---
+++ Assets/Scripts/camera/CameraBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject target;
+
+    Transform targetPos;
+    Transform camPos;
+    Camera camera;
+    float distance;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Transform>().position = new Vector3(-20, 50, -20);
+    }
+    private void Start()
+    {
+        targetPos = target.GetComponent<Transform>();
+        camPos = gameObject.GetComponent<Transform>();
+        camera = gameObject.GetComponent<Camera>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        camPos.LookAt(targetPos);
+
+        distance = target.GetComponent<MiddlePointBetweenPlayers>().getDistanceBetweenPlayers();
+
+        camera.fieldOfView = distance * 1.125f;
+
+        if (camera.fieldOfView < 35)
+        {
+            camera.fieldOfView = 35;
+        }
+        else if (camera.fieldOfView > 60)
+        {
+            camera.fieldOfView = 60;
+        }
+        else
+        {
+            
+        }
+
+
+    }
+    
+}
---
+++ Assets/Scripts/camera/MiddlePointBetweenPlayers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddlePointBetweenPlayers : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    Transform posP1;
+    Transform posP2;
+    Vector3 MiddlePoint;
+
+	// Use this for initialization
+	void Start () {
+
+        posP1 = player1.GetComponent<Transform>();
+        posP2 = player2.GetComponent<Transform>();
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MiddlePoint = (posP1.position + posP2.position) / 2;
+        gameObject.GetComponent<Transform>().position = MiddlePoint;
+	}
+
+    public float getDistanceBetweenPlayers()
+    {
+        float dis;
+        dis = Vector3.Distance(posP1.position, posP2.position);
+        return dis;
+    }
+
+
+
+}
