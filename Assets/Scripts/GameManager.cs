b1cad5c56f3f800a3c6ef666fb094f6eaa252b68
Weikang Hu <Weikang_hu90@hotmail.com>
2017-11-02 14:57:01 +0100

    Added ElevatorCar script, to load the next scene, Added Singelton GameManager, Update Elevator prefab.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/*
+ * Singleton GameManagers contains all guards and all players in a scene
+ */ 
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _instance;
+
+    private GameObject[] guards;
+    private GameObject[] players;
+
+    public GameObject[] Guards { get { return guards; } }
+    public GameObject[] Players { get { return players; } }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+	void Awake () {
+        _instance = this;
+        guards = GameObject.FindGameObjectsWithTag("Guard");
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+}
