ba9207d0b041031d4e024629227b4eda9b970614
Rick Gijsen <rs.gijsen@gmail.com>
2017-12-05 11:14:06 +0100

    Square light prefab, removed cookies

---
+++ Assets/Scripts/Light/resizeLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class resizeLight : MonoBehaviour {
+
+    public Texture2D cookie;
+    public Bounds bounds;
+    Light light;
+    float cookieSize;
+    float biggerSize;
+	// Use this for initialization
+	void Start () {
+        bounds = this.GetComponent<MeshFilter>().mesh.bounds;
+        light = GetComponentInChildren<Light>();
+        cookie = (Texture2D)light.cookie;
+        cookieSize = light.cookieSize;
+        if(this.transform.localScale.x>= this.transform.localScale.z)
+        {
+            print(this.transform.localScale.x);
+            biggerSize = this.transform.localScale.x;
+        }
+        else
+        {
+            print(this.transform.localScale.z);
+            biggerSize = this.transform.localScale.z;
+        }
+        light.cookieSize = light.cookieSize * biggerSize;
+
+        light.enabled = true;
+        this.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
