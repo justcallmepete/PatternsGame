30d0d907ff1a8e87211e5e9b2bc86bb200ff1c41
Rick Gijsen <rs.gijsen@gmail.com>
2017-12-06 12:09:48 +0100

    Elevator open, close, arrive and depart animations

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 /*
  * Elevator script opens the elevator if the player has a keycard. 
@@ -10,9 +11,33 @@ public class Elevator : Interactable {
     private Animator anim;
     private bool locked = true;
 
-	// Use this for initialization
-	void Start () {
-        anim = transform.parent.gameObject.GetComponent<Animator>();
+    public bool isExitElevator = true;
+    GameObject[] players;
+    Vector3[] playerPositions;
+    GameObject floor;
+
+
+    // Use this for initialization
+    void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        playerPositions = new Vector3[2];
+        floor = GameObject.FindGameObjectWithTag("Floor");
+        floor.GetComponent < MeshCollider>().enabled = false;
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerPositions[i] = players[i].transform.position;
+            players[i].transform.position = new Vector3(playerPositions[i].x, playerPositions[i].y - 10, playerPositions[i].z);
+        }
+        anim = gameObject.GetComponent<Animator>();
+
+        if (isExitElevator)
+        {
+            anim.runtimeAnimatorController = AssetDatabase.LoadAssetAtPath("Assets/Animations/Elevator_depart.controller", typeof(RuntimeAnimatorController)) as RuntimeAnimatorController;
+        }
+        else
+        {
+            anim.runtimeAnimatorController = AssetDatabase.LoadAssetAtPath("Assets/Animations/Elevator_arrive.controller", typeof(RuntimeAnimatorController)) as RuntimeAnimatorController;
+        }
 	}
 
     public override void OnInteract(GameObject obj)
@@ -35,5 +60,10 @@ public class Elevator : Interactable {
     public void UnlockElevator()
     {
         locked = false;
+    }
+
+    public void elevatorArrived()
+    {       
+        floor.GetComponent<MeshCollider>().enabled = true;
     }
 }
