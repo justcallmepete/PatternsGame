3ed8e22aec041576fb4259d9fe509d628d6c9433
LAPTOP\sly_e <Sly_emenike@Hotmail.com>
2017-12-06 12:15:39 +0100

    Added console, consoleComponent and console prefab.

---
+++ Assets/Scripts/Items/Console.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * This console calls the ConsoleComponent.onConsole() when the user interacts with the console.
+ */
+
+public class Console : Interactable
+{
+    public List<ConsoleComponent> linkedObjects;
+
+    public override void OnInteract(GameObject obj)
+    {
+        base.OnInteract(obj);
+        Debug.Log("OnInteract");
+        for (int i = 0; i < linkedObjects.Count; i++)
+        {
+            linkedObjects[i].onConsole();
+        }
+    }
+}
