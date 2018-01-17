2820edaf1a2cb452a49ded4c257c7d21cbf82c32
Erik Oegema <erikoegema@outlook.com>
2018-01-17 16:48:07 +0100

    basic mainmenu setup

---
+++ Assets/Scripts/DemoMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMenu : MonoBehaviour {
+
+
+    public void click()
+    {
+        Debug.Log("button click");
+    }
+
+}
