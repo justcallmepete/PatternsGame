45dc467daaa318d2976340d0a957692cc07b7f2d
Weikang Hu <Weikang_hu90@hotmail.com>
2018-01-18 10:59:31 +0100

    Return removed, used for testing.

--- Assets/Scripts/DemoMenu.cs
+++ Assets/Scripts/DemoMenu.cs
@@ -19,32 +19,21 @@ public class DemoMenu : MonoBehaviour {
     // pup up menu
     public void LoadNextLevel()
     {
-        print("test button1");
-        return;
         GameManager.Instance.LoadNextScene();
     }
 
     public void LoadCheckpoint()
     {
-        print("test button2");
-        return;
-
         GameManager.Instance.ReloadCheckpoint(0f);
     }
 
     public void ReloadScene()
     {
-        print("test button3");
-        return;
-
         GameManager.Instance.ReloadScene();
     }
 
     public void BackToMainMenu()
     {
-        print("test button4");
-        return;
-
         GameManager.Instance.BackToMainMenu();      
     }
 
