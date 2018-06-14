46952f8e05df436267f90de06c2d8b7b66a4924b
Sly Emenike <sly.emenike@hva.nl>
2018-06-14 16:02:36 +0200

    Added gard mediator

---
+++ Assets/Scripts/AI/GuardMediator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMediator : MonoBehaviour
+{
+    public List<GuardStateMachine> guards;
+
+    public void PlayerStandinInLight(Vector3 position, int guardsToPoint, int guardsOnAlert, float toPointDist, float onAlertDist)
+    {
+        AssignTaskToGuards(position, guardsToPoint, guardsOnAlert, toPointDist, onAlertDist);
+    }
+
+    private void AssignTaskToGuards(Vector3 fromPoint, int guardsToPoint, int guardsOnAlert, float toPointDist,
+        float onAlertDist)
+    {
+        guards.Sort((a, b) => Vector2.Distance(fromPoint, a.transform.position)
+            .CompareTo(
+                Vector2.Distance(fromPoint, b.transform.position)));
+
+        for (int i = 0; i < guardsToPoint; i++)
+        {
+            guards[i].Distract(fromPoint);
+        }
+
+        for (int j = 0; j < guardsOnAlert; j++)
+        {
+            guards[j + guardsToPoint].Distract(guards[j + guardsToPoint].transform.position);
+
+        }
+    }
+}
